eed4794feabf58275c45e547d7f3175373f0d40c
Divinghawk <Paulsadlowski@web.de>
2022-10-07 13:39:01 +0200

    final with comments

--- Game1_Guessing/src/com/Divinghawk/Guessing/Main_Game_Guessing.java
+++ Game1_Guessing/src/com/Divinghawk/Guessing/Main_Game_Guessing.java
@@ -4,23 +4,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main_Game_Guessing {
-    private static final Random r = new Random();
-    private static final int NUMBER = r.nextInt(100) + 1;
+    private static final Random r = new Random(); // Randomize Number
+    private static final int NUMBER = r.nextInt(100) + 1; // Cast Number in Int
     private static int guess = 0;
 
     public static void main(String[] args) {  
-        Scanner player = new Scanner(System.in);
-            System.out.println("number is " + String.valueOf(NUMBER)); //DEBUG
+        Scanner player = new Scanner(System.in); // Player Input
+            System.out.println("number is " + String.valueOf(NUMBER)); // Solution
             while ( guess != NUMBER ) {
             // prompt player for guess
             System.out.println("Guess a number between 1 and 100");
-            guess = player.nextInt();
-            if ( guess > NUMBER ) {
+            guess = player.nextInt(); // player input in int
+            if ( guess > NUMBER ) { //  If player input too high
                 System.out.println("Too high");
-            } else if ( guess < NUMBER ) {
+            } else if ( guess < NUMBER ) { // If player input too low
                 System.out.println("Too low");
             } else {
-                System.out.println("That's right!");
+                System.out.println("That's right!"); // correct player input + exit + inputcloser
                 System.exit(0);
                 player.close();
             }
